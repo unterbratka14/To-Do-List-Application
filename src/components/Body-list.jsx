ce9c71ef6f7d9fa60b5c18db79fc0c9f1fc0cfcc
Almas Ramazanov <ramazanov_almas@list.ru>
2023-09-01 06:19:45 +0600

    The logic for adding the task is done

--- src/components/Body-list.jsx
+++ src/components/Body-list.jsx
@@ -1,11 +1,13 @@
 const BodyList = ({ list }) => {
   return (
-    <div>
-      <h1>
+    <div className="w-full bg-teal-100">
+      <div className="h-full max-w-[1200px] m-auto">
         {list.map((task) => (
-          <h1>{task}</h1>
+          <h2 className="font-primary text-4xl mb-4 w-3/4" key={task.id}>
+            {task.text}
+          </h2> // This loop sets every object-element in the list to be equal to individual object named task for n times (here n is the number of elements in the list of objects). Then this loop returns heading with unique key, and task, that was written in the input.
         ))}
-      </h1>
+      </div>
     </div>
   );
 };
--- src/components/Head-logic.jsx
+++ src/components/Head-logic.jsx
@@ -1,26 +1,39 @@
 import { useState } from "react";
 
-// eslint-disable-next-line react/prop-types
 const HeadLogic = ({ list, setList }) => {
   const [task, setTask] = useState("");
 
   const inputChangeHandler = (event) => {
-    setTask(event.target.value);
+    setTask(event.target.value); // Everytime the user is pressing the button and typing something this function will take control of it, and automatically will change the value of task to the new text
   };
 
   const clickHandler = (event) => {
-    event.preventDefault();
-    setList([...list, task]);
-    setTask("");
+    event.preventDefault(); // This Line is added to prevent the site to reload (rerender) whenever the button is clicked
+    setList([
+      ...list,
+      { text: task, finished: false, id: Math.ceil(Math.random() * 1000) },
+    ]); // Pushing the new value to the list by copying the material that we had earlier, and adding one more array element which is object of specific items. Here id is needed for making unique keys for the tasks
+    setTask(""); // Making task to be equal to the empty string makes it ready to get another task later, Also it automatically vanishes the string line from the input since value of the input is set to the task
   };
 
   return (
-    <div className="w-full h-2/6 bg-blue-400">
-      <input value={task} type="text" onChange={inputChangeHandler} />
-      <button type="button" onClick={clickHandler}>
-        Add Task
-      </button>
-    </div>
+    <div className="h-[400px] bg-lime-100">
+      <div className="max-w-[1200px] h-full m-auto flex justify-center items-center gap-3">
+        <input
+          className="border border-black rounded p-4 h-12"
+          value={task}
+          type="text"
+          onChange={inputChangeHandler}
+        />
+        <button
+          className="w-1/5 border border-black rounded h-12"
+          type="button"
+          onClick={clickHandler}
+        >
+          Add Task
+        </button>
+      </div>
+    </div> //Wrapper Element End
   );
 };
 
